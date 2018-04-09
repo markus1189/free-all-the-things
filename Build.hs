3b5387ccd0988a25b3a3cb1a83c49c07e4f311a1
Markus Hauck <markus1189@gmail.com>
2018-04-09 20:01:28 +0200

    Initialize presentation

---
+++ Build.hs
@@ -0,0 +1,81 @@
+#! /usr/bin/env nix-shell
+-- #! nix-shell deps.nix -i "ghci -fdefer-type-errors"
+#! nix-shell shell.nix -i "runhaskell --ghc-arg=-threaded --ghc-arg=-Wall"
+#! nix-shell --pure
+
+module Main where
+
+import Control.Monad (when)
+import Data.List (isSuffixOf)
+import Development.Shake
+import Development.Shake.FilePath
+
+main :: IO ()
+main = runShakeBuild
+
+buildDir :: FilePath
+buildDir = "result"
+
+myShakeOptions :: ShakeOptions
+myShakeOptions = shakeOptions { shakeLint = Just LintBasic
+                              , shakeReport = ["report.html", "report.json"]
+                              , shakeThreads = 0
+                              , shakeColor = True
+                              }
+
+runShakeBuild :: IO ()
+runShakeBuild = shakeArgs myShakeOptions $ do
+  wantTargets
+  phonyCommands
+  rules
+
+wantTargets :: Rules ()
+wantTargets = do
+  want [buildDir </> "slides.pdf"]
+
+phonyCommands :: Rules ()
+phonyCommands = do
+  phony "clean" (removeFilesAfter buildDir ["//*"])
+
+rules :: Rules ()
+rules = do
+  buildDir </> "slides.pdf" %> \out -> do
+    let inp = out -<.> "tex"
+        theme = map (buildDir </>) ["beamercolorthemecodecentric.sty"
+                                   ,"beamerfontthemecodecentric.sty"
+                                   ,"beamerinnerthemecodecentric.sty"
+                                   ,"beamerouterthemecodecentric.sty"
+                                   ,"beamerthemecodecentric.sty"
+                                   ]
+    need (inp : (buildDir </> "mindmap.tex") : (buildDir </> "font.tex") : theme)
+    latexmk inp
+
+  buildDir </> "font.tex" %> \_ -> dumpFontFile
+
+  buildDir </> "*.tex" %> \out -> do
+    when ("slides.tex" `isSuffixOf` out) $ do
+      dumpFontFile
+      return ()
+    copyFileChanged (dropDirectory1 out) out
+
+  buildDir </> "*.sty" %> \out -> do
+    copyFileChanged (dropDirectory1 out) out
+
+latexmk :: FilePath -> Action ()
+latexmk inp = do
+  cmd [Cwd (takeDirectory inp)
+      ,WithStdout True
+      ,EchoStdout False
+      ,EchoStderr False
+      ,Stdin ""
+      ] bin ["-g", "-shell-escape", "-pdfxe", dropDirectory1 inp]
+  where bin = "latexmk" :: String
+
+dumpFontFile :: Action ()
+dumpFontFile = do
+  putNormal ("dumping file to " ++ (buildDir </> "font.tex"))
+  -- Guaranteed to be present via `shell.nix`, although this couples shake and nix...
+  Just useCodecentricFont <- getEnv "USE_CC_FONT"
+  let filename = if useCodecentricFont == "true" then "font_cc.tex" else "font_non_cc.tex"
+      outname = (buildDir </> "font.tex")
+  copyFile' filename outname
---
+++ shell.nix
@@ -0,0 +1,71 @@
+{ pkgs ? import <nixpkgs> {}
+, useCodecentricFont ? false
+}: with pkgs;
+
+let
+  codecentricFont = import ./cc-font.nix { inherit pkgs; };
+  ghcPackages = ghc.withPackages (p: with p;
+    [
+      bytestring
+      dhall
+      hlint
+      lens
+      pandoc
+      shake
+      text
+      wreq
+    ]
+  );
+  latexPackages = texlive.combine {
+    inherit (texlive)
+    animate
+    babel
+    beamer
+    chngcntr
+    cleveref
+    enumitem
+    environ
+    etoolbox
+    excludeonly
+    fancyvrb
+    fvextra
+    float
+    framed
+    ifplatform
+    lineno
+    listings
+    mdframed
+    media9
+    microtype
+    minted
+    needspace
+    ocgx2
+    pgf
+    scheme-medium
+    tcolorbox
+    textpos
+    todonotes
+    trimspaces
+    upquote
+    xcolor
+    xstring
+    ;
+  };
+  pyPackages = with pythonPackages; [ pygments ];
+in
+  runCommand "slides-build-input" {
+    FONTCONFIG_FILE = makeFontsConf { fontDirectories = [ google-fonts ubuntu_font_family ] ++ (if useCodecentricFont then [codecentricFont] else []); };
+
+    buildInputs = [
+      coreutils
+      eject
+      ghcPackages
+      graphviz
+      imagemagick
+      latexPackages
+      unzip
+      which
+    ] ++ pyPackages;
+
+    USE_CC_FONT = lib.boolToString useCodecentricFont;
+  } ""
