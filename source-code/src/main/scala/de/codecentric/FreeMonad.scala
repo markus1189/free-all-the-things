ea894843411c79ea8034a23cbb9a46e9f5501885
Markus Hauck <markus1189@gmail.com>
2018-04-15 10:06:48 +0200

    Free Monads snippet

--- source-code/src/main/scala/de/codecentric/FreeMonad.scala
+++ source-code/src/main/scala/de/codecentric/FreeMonad.scala
@@ -1,8 +1,17 @@
 package de.codecentric
 
-//monad typeclass
+//snippet:monad typeclass
 trait Monad[F[_]] {
   def pure[A](x: A): F[A]
   def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 }
+//end
+
+//snippet:free monad
+sealed abstract class Free[F[_], A]
+
+final case class Pure[F[_], A](a: A) extends Free[F, A]
+
+final case class FlatMap[F[_], A, B](
+  fa: Free[F, A], f: A => Free[F, B]) extends Free[F, B]
 //end
