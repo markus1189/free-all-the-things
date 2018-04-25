fffccf514e49b7539cd5ba8b61637232daba3880
Markus Hauck <markus1189@gmail.com>
2018-04-25 21:47:20 +0200

    Free Boolean Algebra

--- source-code/src/main/scala/de/codecentric/FreeBooleanAlgebra.scala
+++ source-code/src/main/scala/de/codecentric/FreeBooleanAlgebra.scala
@@ -52,7 +52,7 @@ object FreeBool {
   }
   //end
 
-  def lift[A](v: A): FreeBool[A] = FreeBool.Inject(v)
+  def inject[A](v: A): FreeBool[A] = FreeBool.Inject(v)
   def and[A](lhs: FreeBool[A], rhs: FreeBool[A]): FreeBool[A] = And(lhs, rhs)
   def or[A](lhs: FreeBool[A], rhs: FreeBool[A]): FreeBool[A] = Or(lhs, rhs)
   def not[A](fb: FreeBool[A]): FreeBool[A] = Not(fb)
@@ -60,18 +60,42 @@ object FreeBool {
   val fls: FreeBool[Nothing] = Fls
 }
 
-sealed trait Predicate
-case class Greater(value: Int) extends Predicate
-case class LessThan(value: Int) extends Predicate
-case class Equals(value: Int) extends Predicate
+object FreeBoolSugar {
+  import FreeBool._
+  implicit def toFreeBoolOps[A](v: FreeBool[A]) = new FreeBoolOps(v)
+
+  sealed class FreeBoolOps[A](self: FreeBool[A]) {
+    def &(other: FreeBool[A]): FreeBool[A] = And(self, other)
+    def |(other: FreeBool[A]): FreeBool[A] = Or(self, other)
+    def unary_! : FreeBool[A] = Not(self)
+  }
+
+}
 
 object FreeBoolDsl {
   import FreeBool._
+  import FreeBoolSugar._
 
-  val predicate = Not(And(Inject(Greater(5)), Inject(LessThan(10))))
+  //snippet:predicate
+  sealed trait Predicate
+  case class Greater(value: Int) extends Predicate
+  case class LessThan(value: Int) extends Predicate
+  case class Equals(value: Int) extends Predicate
 
+  def greaterThan(i: Int): FreeBool[Predicate] = Inject(Greater(i))
+  def lessThan(i: Int): FreeBool[Predicate] = Inject(LessThan(i))
+  def equals(i: Int): FreeBool[Predicate] = Inject(Equals(i))
 
-  def evalInt(input: Int)(pred: FreeBool[Predicate]): Boolean = {
+  def oneOf(is: List[Int]) = is.foldLeft(Fls: FreeBool[Predicate])((acc,i) => acc | equals(i))
+  //end
+
+  def positive = greaterThan(0)
+
+  //snippet:example predicate
+  val predicate: FreeBool[Predicate] = !(greaterThan(5) & lessThan(10)) & !(oneOf(List(42, 1337, 0)))
+  //end
+
+  def evalInt(pred: FreeBool[Predicate])(input: Int): Boolean = {
     def f(p: Predicate): Boolean = p match {
       case Greater(lowerBound) => input > lowerBound
       case LessThan(upperBound) => input < upperBound
@@ -80,4 +104,65 @@ object FreeBoolDsl {
 
     runFreeBool(f, pred)
   }
+
+  val result = evalInt(predicate)(7)
+}
+
+object FreeBoolPartial extends App {
+  import FreeBoolDsl._
+  import FreeBool._
+  import FreeBoolSugar._
+
+  sealed trait Predicate
+  case class OlderThan(i: Int) extends Predicate
+  case class MinSalary(i: Int) extends Predicate
+  case class HasSkill(s: String) extends Predicate
+
+  def olderThan(i: Int): FreeBool[Predicate] = Inject(OlderThan(i))
+  def minSalary(i: Int): FreeBool[Predicate] = Inject(MinSalary(i))
+  def hasSkill(s: String): FreeBool[Predicate] = Inject(HasSkill(s))
+
+  def runFreeBoolP[A,B](f: A => Option[B], fb: FreeBool[A])(implicit B: BoolAlgebra[B]): Either[FreeBool[A], B] = {
+    fb match {
+      case Tru => Right(B.tru)
+      case Fls => Right(B.fls)
+      case Inject(v) => f(v).toRight(fb)
+      case Not(v) => runFreeBoolP(f, v).map(B.not(_))
+      case Or(lhs, rhs) =>
+        val l = runFreeBoolP(f, lhs)
+        val r = runFreeBoolP(f, rhs)
+
+        (l, r) match {
+          case (Right(a), Right(b)) => Right(B.or(a,b))
+          case (Right(a), _) if a == B.tru=> Right(B.tru)
+          case (Right(_), b) => b
+          case (_, Right(b)) if b == B.tru => Right(B.tru)
+          case (a, Right(_)) => a
+          case _ => Left(fb)
+        }
+      case And(lhs, rhs) =>
+        val l = runFreeBoolP(f, lhs)
+        val r = runFreeBoolP(f, rhs)
+
+        (l, r) match {
+          case (Right(a), Right(b)) => Right(B.and(a,b))
+          case (Right(a), _) if a == B.fls=> Right(B.fls)
+          case (Right(_), b) => b
+          case (_, Right(b)) if b == B.fls => Right(B.fls)
+          case (a, Right(_)) => a
+          case _ => Left(fb)
+        }
+    }
+  }
+
+  val p: FreeBool[Predicate] = !olderThan(80) & minSalary(100) & hasSkill("Scala") & hasSkill("DevOps")
+
+  def partially(age: Int, skills: List[String])(p: Predicate): Option[Boolean] = p match {
+    case OlderThan(i) => Some(age > i)
+    case HasSkill(s) => Some(skills.contains(s))
+    case _ => None
+  }
+
+  println(runFreeBoolP(partially(28, List("Scala", "Haskell", "FP")), p))
+  println(runFreeBoolP(partially(28, List("Scala", "Haskell", "DevOps")), p))
 }
